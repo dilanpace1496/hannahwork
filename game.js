a81a592b7bfc7a38c987f4ca89d5986180d5fe0d
Dilan <dilanpace1496@gmail.com>
2019-01-17 22:12:55 -0500

    IT WORKS!!!!!

--- game.js
+++ game.js
@@ -1,10 +1,16 @@
 
-var x = 20;
-var y = 20;
+var x = 0;
+var y = 0;
+var speedx = 2;
+var speedy = 2;
+
+var gravity = 0.3;
+
 
 var img;
 function preload() {
   img = loadImage('Assets/NewPiskel.png');
+  imgg = loadImage('Assets/NewPiskelLeft.png');
 }
 
 function setup() {
@@ -13,29 +19,51 @@ function setup() {
   frameRate(60);
   ellipseMode(RADIUS);
 
-  image(img, 0, 0);
+
 
 }
 
 function draw() {
   background(102);
-
+  x = x + speedx;
+  y = y + speedy;
 
   if (keyIsDown(LEFT_ARROW)){
-    x-=2;
+    speedx = -2;
   }
   if (keyIsDown(RIGHT_ARROW)){
-    x+=2;
+    speedx = 2;
   }
   if (keyIsDown(UP_ARROW)){
-    y-=2;
+    speedy = -2;
   }
   if (keyIsDown(DOWN_ARROW)){
-    y+=2;
+    speedy = 2;
   }
 
-  
+  speedy = speedy + gravity;
 
+
+  // Remember, || means "or."
+  if ((x > width - 70) || (x < 0)) {
+    // If the object reaches either edge, multiply speed by -1 to turn it around.
+    speedx = speedx * -1;
+  }
+
+  if ((y > height - 70) || (y < 0)) {
+    // If the object reaches either edge, multiply speed by -1 to turn it around.
+    speedy = speedy * -0.95;
+    
+  }
+
+ 
+
+  
+if (speedx > 0){
+  image(img, x, y, 100, 100);
+}else if (speedx < 0) {
+  image(imgg, x, y, 100, 100);
+}
 
 }
 
